a1897bea8da4042b614ea416855fd397b6bee80e
YuliannaG <yulianna.iva@gmail.com>
2023-03-26 16:29:22 -0400

    web+nn

--- cifar10_web_app/app.py
+++ cifar10_web_app/app.py
@@ -1,18 +1,44 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, render_template, request
+from imageio.v2 import imread
+from skimage.transform import resize
+import sys
 import os
+
+sys.path.append(os.path.abspath("./model"))
+from load_model import *
+
+graph = tf.compat.v1.get_default_graph()
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html', os=os)
 
-@app.route('/upload', methods=['POST'])
-def upload():
+@app.route('/')
+def index_view():
+    return render_template('index.html')
+
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def predict():
     file = request.files['file']
-    filename = file.filename
+    filename = 'output.png'
     file.save(os.path.join('static', 'uploads', filename))
-    return redirect('/')
+    x = imread('static/uploads/output.png', pilmode='L')
+    x = np.invert(x)
+    x = resize(x, (32, 32, 3))
+    print(x.shape)
+    x = x.reshape(1, 32, 32, 3)
+
+    # image.reshape(3, 32, 32).transpose(1, 2, 0)
+
+    with graph.as_default():
+        model = init()
+        out = model.predict(x)
+        print(out)
+        print(np.argmax(out, axis=1))
+
+        response = np.array_str(np.argmax(out, axis=1))
+        return response
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
--- cifar10_web_app/nn.py
+++ cifar10_web_app/nn.py
@@ -6,39 +6,15 @@ import os
 from keras.utils import np_utils
 from keras.layers import Conv2D, Flatten
 
-
-# def unpickle(file):
-#     import pickle
-#     with open(file, 'rb') as fo:
-#         dict = pickle.load(fo, encoding='latin')
-#     return dict
-#
-#
-# def load_batch_file(batch_filename):
-#     filepath = os.path.join('input/cifar-10-batches-py/', batch_filename)
-#     unpickled = unpickle(filepath)
-#     return unpickled
-#
-# train_batch_1 = load_batch_file('data_batch_1')
-# train_batch_2 = load_batch_file('data_batch_2')
-# train_batch_3 = load_batch_file('data_batch_3')
-# train_batch_4 = load_batch_file('data_batch_4')
-# train_batch_5 = load_batch_file('data_batch_5')
-# test_batch = load_batch_file('test_batch')
-
 (train_x, train_y), (test_x, test_y) = cifar10.load_data()
 
 num_classes = 10
 train_y = np_utils.to_categorical(train_y)
 test_y = np_utils.to_categorical(test_y, num_classes)
 
-
-# train_x = np.concatenate([train_batch_1['data'], train_batch_2['data'], train_batch_3['data'], train_batch_4['data'], train_batch_5['data']])
 train_x = train_x.astype('float32') # this is necessary for the division below
 train_x /= 255
-# train_y = np.concatenate([np_utils.to_categorical(labels, num_classes) for labels in [train_batch_1['labels'], train_batch_2['labels'], train_batch_3['labels'], train_batch_4['labels'], train_batch_5['labels']]])
 test_x = test_x.astype('float32') / 255
-# test_y = np_utils.to_categorical(test_batch['labels'], num_classes)
 
 img_rows = img_cols = 32
 channels = 3
